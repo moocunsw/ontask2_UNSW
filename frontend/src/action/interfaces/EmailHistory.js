c653de18994f28d70cfa07f375318ee544d60fbd
David Phan <z5019338@ad.unsw.edu.au>
2020-10-30 17:52:51 +1100

    emai settings changes

--- frontend/src/action/interfaces/EmailHistory.js
+++ frontend/src/action/interfaces/EmailHistory.js
@@ -1,16 +1,11 @@
 import React from "react";
 import {
-  Form,
   Button,
-  Alert,
-  Spin,
   Icon,
   // Tooltip,
   Divider,
   Table,
   Modal,
-  Popover,
-  notification
 } from "antd";
 import moment from "moment";
 // import _ from "lodash";
@@ -19,9 +14,9 @@ import moment from "moment";
 
 // import SchedulerModal from "../../scheduler/SchedulerModal";
 import EmailSettings from "./EmailSettings";
-
+import EmailJobDetails from "./EmailJobDetails";
 import apiRequest from "../../shared/apiRequest";
-
+const { Parser } = require('json2csv');
 // const FormItem = Form.Item;
 
 class EmailHistory extends React.Component {
@@ -30,9 +25,9 @@ class EmailHistory extends React.Component {
     const { action } = props;
 
     const options = [];
-    action.options.modules.forEach(step => {
+    action.options.modules.forEach((step) => {
       if (step.type === "datasource") {
-        step.fields.forEach(field => {
+        step.fields.forEach((field) => {
           options.push(field);
         });
       }
@@ -44,9 +39,8 @@ class EmailHistory extends React.Component {
       previewing: true,
       sending: false,
       options,
-      emailView: { visible: false },
       emailLocked: true,
-      intervalId: null
+      intervalId: null,
     };
 
     this.dayMap = {
@@ -56,127 +50,50 @@ class EmailHistory extends React.Component {
       thu: { order: 3, label: "Thursday" },
       fri: { order: 4, label: "Friday" },
       sat: { order: 5, label: "Saturday" },
-      sun: { order: 6, label: "Sunday" }
+      sun: { order: 6, label: "Sunday" },
     };
   }
 
-  FeedbackDetails = record => (
-    <div>
-      <b>Feedback provided on:</b>
-      <div>{moment(record.feedback_datetime).format("DD/MM/YYYY, HH:mm")}</div>
+  EmailJobDetails = (job) => {
+    return (
+      <EmailJobDetails job={job} />
+    );
+  };
 
-      <Divider style={{ margin: "6px 0" }} />
-
-      {record.list_feedback && (
-        <div>
-          <b>Dropdown feedback:</b>
-          <div>{record.list_feedback}</div>
-        </div>
-      )}
-
-      {record.list_feedback && record.textbox_feedback && (
-        <Divider style={{ margin: "6px 0" }} />
-      )}
-
-      {record.textbox_feedback && (
-        <div style={{ maxWidth: 400, wordBreak: "break-word" }}>
-          <b>Textbox feedback:</b>
-          <div>{record.textbox_feedback}</div>
-        </div>
-      )}
-    </div>
-  );
-
-  TrackingDetails = record => (
-    <div>
-      <b>First tracked:</b>
-      <div>{moment(record.first_tracked).format("DD/MM/YYYY, HH:mm")}</div>
-      <Divider style={{ margin: "6px 0" }} />
-      <b>Last tracked:</b>
-      <div>
-        {record.last_tracked
-          ? moment(record.last_tracked).format("DD/MM/YYYY, HH:mm")
-          : "N/A"}
-      </div>
-    </div>
-  );
-
-  EmailJobDetails = job => (
-    <Table
-      size="small"
-      columns={[
-        { title: "Recipient", dataIndex: "recipient", key: "recipient" },
-        {
-          title: "Feedback",
-          sorter: (a, b) =>
-            (a.feedback_datetime || "").localeCompare(
-              b.feedback_datetime || ""
-            ),
-          render: (text, record) => {
-            if (!job.included_feedback) return <Icon type="minus" />;
-
-            var feedback =
-              record.list_feedback && record.textbox_feedback
-                ? `["${record.list_feedback}", "${record.textbox_feedback}"]`
-                : record.list_feedback || record.textbox_feedback || "";
-
-            return (
-              <Popover content={this.FeedbackDetails(record)} trigger="hover">
-                {feedback.length > 25
-                  ? `${feedback.slice(0, 25)} ...`
-                  : feedback}
-              </Popover>
-            );
-          }
-        },
-        {
-          title: "Tracking",
-          dataIndex: "track_count",
-          key: "track_count",
-          sorter: (a, b) => (a.track_count > b.track_count ? 1 : -1),
-          render: (count, record) =>
-            count > 0 ? (
-              <Popover content={this.TrackingDetails(record)} trigger="hover">
-                {count}
-              </Popover>
-            ) : (
-              <Icon type="close" />
-            )
-        },
-        {
-          title: "Content",
-          dataIndex: "content",
-          key: "content",
-          render: (text, record) => (
-            <span
-              style={{ cursor: "pointer", color: "#2196F3" }}
-              onClick={() =>
-                this.setState({
-                  emailView: {
-                    visible: true,
-                    recipient: record.recipient,
-                    subject: job.subject,
-                    text
-                  }
-                })
-              }
-            >
-              View
-            </span>
-          )
+  export = () => {
+    const rows = [];
+    const { action } = this.props
+    for (let job of action.emailJobs) {
+      for (let email of job.emails) {
+        let object = {
+          recipient: email.recipient,
+          track_count: email.track_count,
+          first_tracked: email.first_tracked,
+          last_tracked: email.last_tracked,
+          content: email.content,
+          subject: job.subject,
+          included_feedback: job.included_feedback,
+          action_name: action.name,
+          action_id: action.id,
+          reply_to_email: action.emailSettings.replyTo,
+          reply_to_name: action.emailSettings.fromName,
         }
-      ]}
-      dataSource={job.emails}
-      rowKey="email_id"
-      pagination={{ size: "small", defaultPageSize: 10, showSizeChanger: true, pageSizeOptions: ['10', '25', '50']}}
-    />
-  );
+        rows.push(object)
+      }
+    }
+    const json2csvParser = new Parser();
+    const csv = json2csvParser.parse(rows);
+    var pom = document.createElement('a');
+    var blob = new Blob([csv],{type: 'text/csv;charset=utf-8;'});
+    var url = URL.createObjectURL(blob);
+    pom.href = url;
+    let filename = action.name.replace(" ", "_") + action.id + ".csv"
+    pom.setAttribute('download', filename);
+    pom.click();
+  }
 
   EmailHistory = () => {
     const { action } = this.props;
-    const { emailView } = this.state;
-
-    const onCancel = () => this.setState({ emailView: { visible: false } });
 
     return (
       <div>
@@ -189,67 +106,53 @@ class EmailHistory extends React.Component {
               title: "Date/Time",
               dataIndex: "initiated_at",
               key: "initiated_at",
-              render: text => moment(text).format("DD/MM/YYYY, HH:mm")
+              render: (text) => moment(text).format("DD/MM/YYYY, HH:mm"),
             },
             { title: "Type", dataIndex: "type", key: "type" },
             { title: "Subject", dataIndex: "subject", key: "subject" },
             {
+              title: "Action",
+              dataIndex: "action",
+              key: "action",
+              render: () => {
+                return <span>{action.name}</span>;
+              },
+            },
+            {
+              title: "Reply To",
+              dataIndex: "replyTo",
+              key: "replyTo",
+              render: () => {
+                return <span>{action.emailSettings.replyTo}</span>;
+              },
+            },
+            {
               title: "Feedback",
               dataIndex: "included_feedback",
               key: "included_feedback",
-              render: text =>
-                text ? <Icon type="check" /> : <Icon type="close" />
+              render: (text) =>
+                text ? <Icon type="check" /> : <Icon type="close" />,
             },
             {
               title: "Tracking",
               render: (text, record) => {
                 const trackedCount = record.emails.filter(
-                  email => !!email.first_tracked
+                  (email) => !!email.first_tracked
                 ).length;
                 const trackedPct = Math.round(
                   (trackedCount / record.emails.length) * 100
                 );
                 return (
-                  <span>{`${trackedCount} of ${
-                    record.emails.length
-                  } (${trackedPct}%)`}</span>
+                  <span>{`${trackedCount} of ${record.emails.length} (${trackedPct}%)`}</span>
                 );
-              }
-            }
+              },
+            },
           ]}
           dataSource={action.emailJobs}
           expandedRowRender={this.EmailJobDetails}
           rowKey="job_id"
           pagination={{ size: "small", pageSize: 5 }}
         />
-
-        <Modal
-          visible={emailView.visible}
-          onCancel={onCancel}
-          footer={
-            <Button type="primary" onClick={onCancel}>
-              OK
-            </Button>
-          }
-        >
-          <div className="view_email">
-            <div className="field">Recipient:</div>
-            <div className="value">{emailView.recipient}</div>
-            <div className="field">Subject:</div>
-            <div className="value">{emailView.subject}</div>
-            <div className="field">Date/Time:</div>
-            <div className="value">
-              {moment(emailView.initiated_at).format("DD/MM/YYYY, HH:mm")}
-            </div>
-            <Divider />
-            <div
-              className="email_content"
-              dangerouslySetInnerHTML={{
-                __html: emailView.text
-              }}
-            />
-          </div>
-        </Modal>
       </div>
     );
   };
@@ -264,13 +167,18 @@ class EmailHistory extends React.Component {
       options,
       index,
       populatedContent,
-      emailLocked
+      emailLocked,
     } = this.state;
+
+    console.log(action);
 
     return (
       <div className="email">
         <div>
           <h3>Email history</h3>
+          <Button type="primary" style={{ marginBottom: 5 }} onClick={() => this.export()}>
+            Export to CSV
+          </Button>
           {this.EmailHistory()}
         </div>
       </div>
---
+++ frontend/src/action/interfaces/EmailJobDetails.js
@@ -0,0 +1,209 @@
+import React from "react";
+import {
+  Icon,
+  // Tooltip,
+  Table,
+  Input,
+  Popover,
+  Divider,
+  Modal,
+  Button,
+} from "antd";
+import moment from "moment";
+import Highlighter from "react-highlight-words";
+
+class EmailJobDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: "",
+      emailView: { visible: false },
+    };
+  }
+
+  searchEntry = (dataSource, searchText) => {
+    let searchFiltered = dataSource.filter(
+      (it) =>
+        !it.recipient ||
+        (it.recipient && it.recipient.toLowerCase().includes(searchText))
+    );
+    return searchFiltered;
+  };
+
+  FeedbackDetails = (record) => (
+    <div>
+      <b>Feedback provided on:</b>
+      <div>{moment(record.feedback_datetime).format("DD/MM/YYYY, HH:mm")}</div>
+
+      <Divider style={{ margin: "6px 0" }} />
+
+      {record.list_feedback && (
+        <div>
+          <b>Dropdown feedback:</b>
+          <div>{record.list_feedback}</div>
+        </div>
+      )}
+
+      {record.list_feedback && record.textbox_feedback && (
+        <Divider style={{ margin: "6px 0" }} />
+      )}
+
+      {record.textbox_feedback && (
+        <div style={{ maxWidth: 400, wordBreak: "break-word" }}>
+          <b>Textbox feedback:</b>
+          <div>{record.textbox_feedback}</div>
+        </div>
+      )}
+    </div>
+  );
+
+  onCancel = () => this.setState({ emailView: { visible: false } });
+
+  TrackingDetails = (record) => (
+    <div>
+      <b>First tracked:</b>
+      <div>{moment(record.first_tracked).format("DD/MM/YYYY, HH:mm")}</div>
+      <Divider style={{ margin: "6px 0" }} />
+      <b>Last tracked:</b>
+      <div>
+        {record.last_tracked
+          ? moment(record.last_tracked).format("DD/MM/YYYY, HH:mm")
+          : "N/A"}
+      </div>
+    </div>
+  );
+
+  render = () => {
+    const { job } = this.props;
+    const { searchText, emailView } = this.state;
+
+    return (
+      <div>
+        <Input
+          placeholder="Search by Recipient"
+          prefix={<Icon type="search" style={{ color: "rgba(0,0,0,.25)" }} />}
+          onChange={(e) => {
+            this.setState({ searchText: e.target.value.toLowerCase() });
+          }}
+        />
+        <Table
+          size="small"
+          columns={[
+            { title: "Recipient", dataIndex: "recipient", key: "recipient",
+              render: (text) => (
+                <Highlighter
+                  highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+                  searchWords={[searchText]}
+                  autoEscape
+                  textToHighlight={text.toString()}
+                />
+              )
+            },
+            {
+              title: "Feedback",
+              sorter: (a, b) =>
+                (a.feedback_datetime || "").localeCompare(
+                  b.feedback_datetime || ""
+                ),
+              render: (text, record) => {
+                if (!job.included_feedback) return <Icon type="minus" />;
+
+                var feedback =
+                  record.list_feedback && record.textbox_feedback
+                    ? `["${record.list_feedback}", "${record.textbox_feedback}"]`
+                    : record.list_feedback || record.textbox_feedback || "";
+
+                return (
+                  <Popover
+                    content={this.FeedbackDetails(record)}
+                    trigger="hover"
+                  >
+                    {feedback.length > 25
+                      ? `${feedback.slice(0, 25)} ...`
+                      : feedback}
+                  </Popover>
+                );
+              },
+            },
+            {
+              title: "Tracking",
+              dataIndex: "track_count",
+              key: "track_count",
+              sorter: (a, b) => (a.track_count > b.track_count ? 1 : -1),
+              render: (count, record) =>
+                count > 0 ? (
+                  <Popover
+                    content={this.TrackingDetails(record)}
+                    trigger="hover"
+                  >
+                    {count}
+                  </Popover>
+                ) : (
+                  <Icon type="close" />
+                ),
+            },
+            {
+              title: "Content",
+              dataIndex: "content",
+              key: "content",
+              render: (text, record) => (
+                <span
+                  style={{ cursor: "pointer", color: "#2196F3" }}
+                  onClick={() =>
+                    this.setState({
+                      emailView: {
+                        visible: true,
+                        recipient: record.recipient,
+                        subject: job.subject,
+                        text,
+                      },
+                    })
+                  }
+                >
+                  View
+                </span>
+              ),
+            },
+          ]}
+          dataSource={this.searchEntry(job.emails, searchText)}
+          rowKey="email_id"
+          pagination={{
+            size: "small",
+            defaultPageSize: 10,
+            showSizeChanger: true,
+            pageSizeOptions: ["10", "25", "50"],
+          }}
+        />
+         <Modal
+          visible={emailView.visible}
+          onCancel={this.onCancel}
+          footer={
+            <Button type="primary" onClick={this.onCancel}>
+              OK
+            </Button>
+          }
+        >
+          <div className="view_email">
+            <div className="field">Recipient:</div>
+            <div className="value">{emailView.recipient}</div>
+            <div className="field">Subject:</div>
+            <div className="value">{emailView.subject}</div>
+            <div className="field">Date/Time:</div>
+            <div className="value">
+              {moment(emailView.initiated_at).format("DD/MM/YYYY, HH:mm")}
+            </div>
+            <Divider />
+            <div
+              className="email_content"
+              dangerouslySetInnerHTML={{
+                __html: emailView.text,
+              }}
+            />
+          </div>
+        </Modal>
+      </div>
+    );
+  };
+}
+
+export default EmailJobDetails;
